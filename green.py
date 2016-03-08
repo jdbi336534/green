c555da5b7596339fbcaf9cecf506f2b88a114c56
ahangchen <1418659400@qq.com>
2016-03-08 09:43:24 +0800

    open test code

--- green.py
+++ green.py
@@ -36,5 +36,5 @@ def daily_commit(start_date, end_date):
         trick_commit(cur_date.year, cur_date.month, cur_date.day)
 
 
-# if __name__ == '__main__':
-    # daily_commit(datetime.date(2015, 3, 31), datetime.date(2016, 1, 28))
+if __name__ == '__main__':
+    daily_commit(datetime.date(2015, 3, 31), datetime.date(2016, 1, 28))
